1738d39a1e850082fe5e5ea33a23ed9e0ea85f02
Khoa Anh <kohavn>
2022-03-12 14:26:43 +0700

    commit the first version

---
+++ src/data/WorkData.js
@@ -0,0 +1,45 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Vietjet Agency App",
+        description:"An internal app which is used by agency to control reservation system.",
+        tags:["manual","api","database"],
+        
+        demo:"https://www.vietjetair.com/",
+        github:"https://github.com/KhoaKoha"
+    },
+    {
+        id:2,
+        name:"Vietnam khoe manh",
+        description:"An app with same feature as PC-Covid, user can register for Covid test or use for health declaration.",
+        tags:["manual","api"],
+        
+        demo:"https://vietnamkhoemanh.vn/",
+        github:"https://github.com/KhoaKoha"
+        
+    },
+    {
+        id:3,
+        name:"Pet Automation Project",
+        description:"Develop and deploy test case by using Selenium framework, using POM design pattern.",
+        tags:["selenium","testNG","Junit"],
+        
+        demo:"https://demo.nopcommerce.com/",
+        github:"https://github.com/KhoaKoha/nopcommerce-pom"
+    },
+    {
+        id:4,
+        name:"Nahimic",
+        description:"An audio software is a high-definition sound technology that boosts your MSI computer's audio and voice performance",
+        tags:["manual"],
+        
+        demo:"https://www.nahimic.com/",
+        github:"https://github.com/KhoaKoha/nopcommerce-pom"
+       
+    }
+    
+    
+  
+] 
